aebe6415b457da8cb4236bdb8b520d1a430cad47
naatii <jcorber855@g.educaand.es>
2023-01-11 20:22:37 +0100

    relación completada

---
+++ ejercicio16.js
@@ -0,0 +1,4 @@
+var num1 = prompt("Escribe un número")
+var num2 = prompt("Escribe un número")
+var suma = (parseInt (num1) + parseInt(num2)) 
+alert("La suma es: "+suma)
---
+++ ejercicio18.js
@@ -0,0 +1,6 @@
+var num = prompt("escriba un número")
+if (parseInt(num)%2==0){
+    alert("el número es par")
+}else{
+    alert("el número es impar")
+}
