c3957b474ec93df307733db4e1714e694d5e984b
naatii <jcorber855@g.educaand.es>
2023-01-14 18:42:46 +0100

    media relación 2

---
+++ relacion 2/ejercicio4.js
@@ -0,0 +1,16 @@
+var entrada = prompt("Escribe una nota")
+if (entrada<5){
+    alert("Suspenso")
+}
+if (entrada>=5){
+    alert("Aprobado")
+}
+if (entrada==6){
+    alert("Bien")
+}
+if (entrada>7, entrada<10){
+    alert("notable")
+}
+if (entrada==10){
+    alert("Sobresaliente")
+}
